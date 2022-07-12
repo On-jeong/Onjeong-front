77e3a2bf0f988beeab1ad113d9b3524e34bbc8e5
hyeonjin25 <shj41738866@gmail.com>
2022-07-12 17:32:01 +0900

    로그인, 회원가입 validation check

--- src/screens/sign/SignInScreen.js
+++ src/screens/sign/SignInScreen.js
@@ -1,10 +1,115 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import NoHeader from '@/components/NoHeader';
+import {FontStyle} from '@/utils/GlobalFonts';
+import {AppColors} from '@/utils/GlobalStyles';
+import {
+  Box,
+  Container,
+  InputBox,
+  InputContainer,
+  SignButton,
+  Title,
+} from './SignUpScreen';
 
-const SignInScreen = () => {
+const SignInScreen = ({naviagtion}) => {
+  const [inputCheck, setInputCheck] = useState(false);
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [pwCheck, setPwCheck] = useState('');
+  const [birth, setBirth] = useState('');
+  const [role, setRole] = useState('');
+  const [invite, setInvite] = useState('');
+
+   // 항목을 전부 입력했는지 체크
+  useEffect(() => {
+    if (id && pw && pwCheck && birth && role) setInputCheck(true);
+    else setInputCheck(false);
+  }, [id, pw, pwCheck, birth, role]);
+
+  const onSubmit = () => {
+    console.log('id: ' + id);
+    console.log('pw: ' + pw);
+    console.log('pwCheck: ' + pwCheck);
+    console.log('birth: ' + birth);
+    console.log('role: ' + role);
+    console.log('invite: ' + invite);
+
+    if (!id) {
+      alert('아이디를 입력해주세요.');
+      return 0;
+    } else if (pw != pwCheck) {
+      alert('비밀번호 확인이 맞지 않습니다.');
+      return 0;
+    } else if (pw != pwCheck) {
+      alert('비밀번호 확인을 입력해주세요.');
+      return 0;
+    } else if (pw != pwCheck) {
+      alert('생일을 입력해주세요.');
+      return 0;
+    } else if (pw != pwCheck) {
+      alert('가족 내 역할을 입력해주세요.');
+      return 0;
+    }
+  };
+
   return (
-    <>
-    </>
-  )
-}
+    <NoHeader isBack={true} naviagtion={naviagtion}>
+      <Container>
+        <Box>
+          <Title>
+            <FontStyle.Big>온정</FontStyle.Big>
+          </Title>
+          <InputContainer>
+            <InputBox
+              maxLength={15}
+              placeholder="아이디"
+              value={id}
+              onChangeText={setId}
+            />
+            <InputBox
+              maxLength={15}
+              placeholder="비밀번호"
+              value={pw}
+              onChangeText={setPw}
+              secureTextEntry={true}
+            />
+            <InputBox
+              maxLength={15}
+              placeholder="비밀번호 확인"
+              value={pwCheck}
+              onChangeText={setPwCheck}
+              secureTextEntry={true}
+            />
+            <InputBox
+              maxLength={15}
+              placeholder="생일"
+              value={birth}
+              onChangeText={setBirth}
+            />
+            <InputBox
+              maxLength={15}
+              placeholder="가족 내 역할  (ex)첫째 딸"
+              value={role}
+              onChangeText={setRole}
+            />
+            <InputBox
+              maxLength={15}
+              placeholder="초대가족 아이디"
+              value={invite}
+              onChangeText={setInvite}
+            />
+          </InputContainer>
+          <SignButton
+            color={inputCheck ? AppColors.green2 : AppColors.blur}
+            onPress={onSubmit}
+            disabled={!inputCheck}>
+            <FontStyle.ContentB>회원가입</FontStyle.ContentB>
+          </SignButton>
+        </Box>
+      </Container>
+    </NoHeader>
+  );
+};
 
-export default SignInScreen
+export default SignInScreen;
--- src/screens/sign/SignUpScreen.js
+++ src/screens/sign/SignUpScreen.js
@@ -1,35 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import NoHeader from '@/components/NoHeader';
 import {FontStyle} from '@/utils/GlobalFonts';
 import {AppColors} from '@/utils/GlobalStyles';
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled.View`
+export const Box = styled.View`
   width: 100%;
-  height: 60%;
   align-items: center;
-  //background-color: pink;
+  padding-bottom: 30px;
 `;
 
-const Title = styled.View`
+export const Title = styled.View`
   justify-content: center;
   align-items: center;
   margin-bottom: 50px;
 `;
 
-const InputContainer = styled.View`
+export const InputContainer = styled.View`
   width: 100%;
   align-items: center;
   margin-bottom: 20px;
 `;
 
-const InputBox = styled.TextInput`
+export const InputBox = styled.TextInput`
   width: 70%;
   border-bottom-width: 1px;
   border-color: ${AppColors.border};
@@ -38,7 +37,7 @@ const InputBox = styled.TextInput`
   margin-bottom: 10px;
 `;
 
-const SignButton = styled.TouchableOpacity`
+export const SignButton = styled.TouchableOpacity`
   height: 40px;
   width: 70%;
   border-radius: 12px;
@@ -50,29 +49,64 @@ const SignButton = styled.TouchableOpacity`
 `;
 
 const SignUpScreen = ({navigation}) => {
+  const [inputCheck, setInputCheck] = useState(false);
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  // 항목을 전부 입력했는지 체크
+  useEffect(() => {
+    if (id && pw) setInputCheck(true);
+    else setInputCheck(false);
+  }, [id, pw]);
+
+  const onSubmit = () => {
+    console.log('id: ' + id);
+    console.log('pw: ' + pw);
+
+    if (!id) {
+      alert('아이디를 입력해주세요.');
+      return 0;
+    } else if (!pw) {
+      alert('비밀번호를 입력해주세요.');
+      return 0;
+    }
+
+    navigation.navigate('Home');
+  };
+
   return (
     <NoHeader>
       <Container>
-        <Title>
-          <FontStyle.Big>온정</FontStyle.Big>
-        </Title>
         <Box>
+          <Title>
+            <FontStyle.Big>온정</FontStyle.Big>
+          </Title>
           <InputContainer>
-            <InputBox maxLength={15} placeholder="아이디" />
+            <InputBox
+              maxLength={15}
+              placeholder="아이디"
+              value={id}
+              onChangeText={setId}
+            />
             <InputBox
               maxLength={15}
               placeholder="비밀번호"
+              value={pw}
+              onChangeText={setPw}
               secureTextEntry={true}
             />
           </InputContainer>
           <SignButton
-            color={AppColors.red1}
-            onPress={() => {
-              navigation.navigate('Home');
-            }}>
+            color={inputCheck ? AppColors.red1 : AppColors.blur}
+            onPress={onSubmit}
+            disabled={!inputCheck}>
             <FontStyle.ContentB>로그인</FontStyle.ContentB>
           </SignButton>
-          <SignButton color={AppColors.green2}>
+          <SignButton
+            color={AppColors.green2}
+            onPress={() => {
+              navigation.navigate('SignIn');
+            }}>
             <FontStyle.ContentB>회원가입</FontStyle.ContentB>
           </SignButton>
         </Box>
