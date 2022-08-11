28cda7aa43ce3c4e5fee71019bdcf05c615ec247
hyeonjin25 <shj41738866@gmail.com>
2022-08-11 19:09:51 +0900

    asyncstorage hook 추가

---
+++ src/config/storage.js
@@ -0,0 +1,51 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const setItem = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getItem = async key => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const setStrItem = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.parse(value));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getStrItem = async key => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return JSON.stringify(value);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeItem = async key => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const storage = {
+  setItem,
+  setStrItem,
+  getItem,
+  getStrItem,
+  removeItem,
+};
