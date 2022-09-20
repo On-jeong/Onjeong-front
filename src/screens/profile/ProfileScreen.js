040a9b38eb9a3e339b3eaaee59b4a97c050aca66
hyeonjin25 <shj41738866@gmail.com>
2022-09-20 21:16:32 +0900

    프로필과 문답 페이지

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {BasicHeader} from '@/components/WithHeader';
 import styled from 'styled-components';
 import {AppColors, windowHeight, windowWidth} from '@/utils/GlobalStyles';
@@ -6,6 +6,8 @@ import {ScrollView} from 'react-native';
 import {Components} from '@/utils/Components';
 import {useGetFamilyList} from '../../hooks/useProFileData';
 import {FontStyle} from '../../utils/GlobalFonts';
+import {useQueryClient} from '@tanstack/react-query';
+import {useFocusEffect} from '@react-navigation/native';
 
 const ProfileBox = styled.View`
   flex-direction: row;
@@ -26,18 +28,30 @@ const Profile = styled.TouchableOpacity`
   align-items: center;
 `;
 
-export default function ProfileScreen({navigation}) {
-  const {data, isLoading, status} = useGetFamilyList();
+const ProfileScreen = ({navigation}) => {
+  const {data, isLoading, status, error, refetch} = useGetFamilyList();
+  const queryClient = useQueryClient();
+  console.log(status);
+
+  useFocusEffect(
+    useCallback(() => {
+      refetch();
+    }, []),
+  );
 
   return (
     <BasicHeader title="가족 프로필" navigation={navigation}>
       <ScrollView>
         <ProfileBox>
           {isLoading && <FontStyle.Content>Loading...</FontStyle.Content>}
+          {error && (
+            <FontStyle.Content>데이터 로딩에 실패했습니다.</FontStyle.Content>
+          )}
           {status == 'success' && (
             <>
               {data?.data.map(fm => (
                 <Profile
+                  key={fm.userId}
                   onPress={() =>
                     navigation.navigate('ProfileDetail', {
                       userId: fm.userId,
@@ -54,4 +68,6 @@ export default function ProfileScreen({navigation}) {
       </ScrollView>
     </BasicHeader>
   );
-}
+};
+
+export default ProfileScreen;
