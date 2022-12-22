5f83fdb6ad75fc2b4348bfbc9eb8c996f1e7ad93
hyeonjin25 <shj41738866@gmail.com>
2022-12-22 15:31:07 +0900

    코인 적립 가이드 업데이트

--- src/screens/home/CoinGuideScreen.js
+++ src/screens/home/CoinGuideScreen.js
@@ -20,9 +20,17 @@ const CoinGuideScreen = ({navigation}) => {
       title={'영양제 적립 가이드'}
       navigation={navigation}>
       <ScrollView>
+        <Message title={'일일랜덤 코인지급'} coin={'10~100'} />
         <Message title={'메세지 보내기'} coin={10} />
-        <Message title={'게시물 작성'} coin={20} />
-        <Message title={'프로필 등록'} coin={100} />
+        <Message title={'오늘의 기록 작성'} coin={20} />
+        <Message title={'프로필 사진 등록'} coin={100} />
+        <Message title={'상태메시지 등록'} coin={100} />
+        <Message title={'좋아하는 것 작성하기'} coin={100} />
+        <Message title={'싫어하는 것 작성하기'} coin={100} />
+        <Message title={'한단어로 표현하는 것 작성'} coin={100} />
+        <Message title={'관심사 작성'} coin={100} />
+        <Message title={'이주의 문답 작성'} coin={10} />
+        <Message title={'이주의 문답 모든 가족이 작성할 경우'} coin={200} />
         <Components.EmptyBox />
       </ScrollView>
     </NoHeader>
