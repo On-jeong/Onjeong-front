b45fbce9d301142bb9560074cad955f62b2c7b71
hyeonjin25 <shj41738866@gmail.com>
2023-03-12 17:48:11 +0900

    add new flower images

---
+++ src/ui/icons/index.js
@@ -0,0 +1,23 @@
+import IconMailWhite from '@/assets/icons/Icon_mail_white.svg';
+import IconMailBlack from '@/assets/icons/Icon_mail_black.svg';
+import IconQuestWhite from '@/assets/icons/Icon_quest_white.svg';
+import IconQuestBlack from '@/assets/icons/Icon_quest_black.svg';
+import IconProfileWhite from '@/assets/icons/Icon_profile_white.svg';
+import IconProfileBlack from '@/assets/icons/Icon_profile_black.svg';
+import IconCalendarWhite from '@/assets/icons/Icon_calendar_white.svg';
+import IconCalendarBlack from '@/assets/icons/Icon_calendar_black.svg';
+import IconHomeWhite from '@/assets/icons/Icon_home_white.svg';
+import IconHomeBlack from '@/assets/icons/Icon_home_black.svg';
+
+export const AppIcons = {
+  IconMailWhite,
+  IconMailBlack,
+  IconQuestWhite,
+  IconQuestBlack,
+  IconProfileWhite,
+  IconProfileBlack,
+  IconCalendarWhite,
+  IconCalendarBlack,
+  IconHomeWhite,
+  IconHomeBlack,
+};
--- src/utils/FlowerImagePath.js
+++ src/utils/FlowerImagePath.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import Camellia1 from '@/assets/image/flower/camellia/camellia1.svg';
+import Camellia2 from '@/assets/image/flower/camellia/camellia2.svg';
+import Camellia3 from '@/assets/image/flower/camellia/camellia3.svg';
+import Camellia4 from '@/assets/image/flower/camellia/camellia4.svg';
+import Camellia5 from '@/assets/image/flower/camellia/camellia5.svg';
+import Camellia6 from '@/assets/image/flower/camellia/camellia6.svg';
+import Camellia7 from '@/assets/image/flower/camellia/camellia7.svg';
+import Camellia8 from '@/assets/image/flower/camellia/camellia8.svg';
+import Camellia9 from '@/assets/image/flower/camellia/camellia9.svg';
+import Camellia10 from '@/assets/image/flower/camellia/camellia10.svg';
+import Violet1 from '@/assets/image/flower/violet/violet1.svg';
+import Violet2 from '@/assets/image/flower/violet/violet2.svg';
+import Violet3 from '@/assets/image/flower/violet/violet3.svg';
+import Violet4 from '@/assets/image/flower/violet/violet4.svg';
+import Violet5 from '@/assets/image/flower/violet/violet5.svg';
+import Violet6 from '@/assets/image/flower/violet/violet6.svg';
+import Violet7 from '@/assets/image/flower/violet/violet7.svg';
+import Violet8 from '@/assets/image/flower/violet/violet8.svg';
+import Violet9 from '@/assets/image/flower/violet/violet9.svg';
+import Violet10 from '@/assets/image/flower/violet/violet10.svg';
+
 export const seed = [
   '', //0
   require('@/assets/image/flower/seed_1.png'),
@@ -8,15 +30,41 @@ export const seed = [
 export const flower = {
   camellia: [
     '', //0
-    '', //1
-    '', //2
-    '', //3
-    require('@/assets/image/flower/camellia/camellia_4.png'),
-    require('@/assets/image/flower/camellia/camellia_5.png'),
-    require('@/assets/image/flower/camellia/camellia_6.png'),
-    require('@/assets/image/flower/camellia/camellia_7.png'),
-    require('@/assets/image/flower/camellia/camellia_8.png'),
-    require('@/assets/image/flower/camellia/camellia_9.png'),
-    require('@/assets/image/flower/camellia/camellia_10.png'),
+    <Camellia1 />,
+    <Camellia2 />,
+    <Camellia3 />,
+    <Camellia4 />,
+    <Camellia5 />,
+    <Camellia6 />,
+    <Camellia7 />,
+    <Camellia8 />,
+    <Camellia9 />,
+    <Camellia10 />,,
+  ],
+  // cosmos: [
+  //   '', //0
+  //   require('@/assets/image/flower/cosmos/cosmos1.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos2.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos3.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos4.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos5.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos6.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos7.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos8.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos9.svg'),
+  //   require('@/assets/image/flower/cosmos/cosmos10.svg'),
+  // ],
+  violet: [
+    '', //0
+    <Violet1 />,
+    <Violet2 />,
+    <Violet3 />,
+    <Violet4 />,
+    <Violet5 />,
+    <Violet6 />,
+    <Violet7 />,
+    <Violet8 />,
+    <Violet9 />,
+    <Violet10 />,
   ],
 };
