c1b8df6d08951eda9c986fe78e7253e4e8d466f7
hyeonjin25 <shj41738866@gmail.com>
2023-03-17 14:32:23 +0900

    console.log 수정

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 import messaging from '@react-native-firebase/messaging';
 
-//fcm -  background
+//  fcm -  background
 //  remoteMessage.data로 메세지에 접근가능
 //  remoteMessage.from 으로 topic name 또는 message identifier
 //  remoteMessage.messageId 는 메시지 고유값 id
 //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
 //  remoteMessage.sentTime 보낸시간
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('background 알림 도착!', remoteMessage);
+  console.log('background 알림 : ', remoteMessage);
 });
 
 AppRegistry.registerComponent(appName, () => App);
