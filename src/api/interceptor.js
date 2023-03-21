1029db418924199cf9e86e6dd579796486905660
hyeonjin25 <shj41738866@gmail.com>
2023-03-21 20:14:49 +0900

    토큰 에러 헨들링 수정

--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -9,7 +9,7 @@ import customAxios, {refreshAxios} from './axios';
 
 export const Interceptor = ({children}) => {
   const navigation = useNavigation();
-  
+
   customAxios.interceptors.response.use(
     res => res,
     async err => {
@@ -72,11 +72,13 @@ export const Interceptor = ({children}) => {
       }
 
       // 잘못된 토큰
-      if (
-        err?.response?.data.code == 'A005' ||
-        err?.response?.data.code == 'A006'
-      ) {
+      if (err?.response?.data.code == 'A005') {
         alert('잘못 된 토큰입니다! 로그인 화면으로 이동합니다.');
+        navigation.navigate('SignIn');
+      }
+
+      // 없는 토큰
+      if (err?.response?.data.code == 'A006') {
         navigation.navigate('SignIn');
       }
 
