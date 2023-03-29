a7dbc27c133983d53c0b39a7f572fa53da26fd7a
hyeonjin25 <shj41738866@gmail.com>
2023-03-29 22:33:48 +0900

    change icon names

--- src/ui/icons/index.js
+++ src/ui/icons/index.js
@@ -1,30 +1,29 @@
-import MailWhite from '@/assets/icons/Icon_mail_white.svg';
-import MailBlack from '@/assets/icons/Icon_mail_black.svg';
-import QuestWhite from '@/assets/icons/Icon_quest_white.svg';
-import QuestBlack from '@/assets/icons/Icon_quest_black.svg';
-import ProfileWhite from '@/assets/icons/Icon_profile_white.svg';
-import ProfileBlack from '@/assets/icons/Icon_profile_black.svg';
-import CalendarWhite from '@/assets/icons/Icon_calendar_white.svg';
-import CalendarBlack from '@/assets/icons/Icon_calendar_black.svg';
-import HomeWhite from '@/assets/icons/Icon_home_white.svg';
-import HomeBlack from '@/assets/icons/Icon_home_black.svg';
+import MailWhite from '@/assets/icons/mail_white.svg';
+import MailBlack from '@/assets/icons/mail_black.svg';
+import QuestWhite from '@/assets/icons/quest_white.svg';
+import QuestBlack from '@/assets/icons/quest_black.svg';
+import ProfileWhite from '@/assets/icons/profile_white.svg';
+import ProfileBlack from '@/assets/icons/profile_black.svg';
+import CalendarWhite from '@/assets/icons/calendar_white.svg';
+import CalendarBlack from '@/assets/icons/calendar_black.svg';
+import HomeWhite from '@/assets/icons/home_white.svg';
+import HomeBlack from '@/assets/icons/home_black.svg';
 import Right from '@/assets/icons/right.svg';
 import Back from '@/assets/icons/back.svg';
 import Back_gray from '@/assets/icons/back_gray.svg';
 import Down from '@/assets/icons/down.svg';
 
 import Add from '@/assets/icons/add.svg';
-import Bell from '@/assets/icons/Icon_bell.svg';
-import List from '@/assets/icons/Icon_list.svg';
-import Setting from '@/assets/icons/Icon_setting.svg';
-import Trash from '@/assets/icons/Icon_trash.svg';
+import Bell from '@/assets/icons/bell.svg';
+import List from '@/assets/icons/list.svg';
+import Setting from '@/assets/icons/setting.svg';
+import Trash from '@/assets/icons/trash.svg';
 
 import Water from '@/assets/icons/water.svg';
 import Flower from '@/assets/icons/flower.svg';
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
 import styled from 'styled-components';
 
 export const AppIcons = {
