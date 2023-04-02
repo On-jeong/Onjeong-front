0ec30058d551278396d5cd3459a4fe9a074b9075
hyeonjin25 <shj41738866@gmail.com>
2023-04-02 17:38:46 +0900

    focus 자동 이동

--- src/components/inputs/BorderBottomInput.js
+++ src/components/inputs/BorderBottomInput.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import styled from 'styled-components';
 import {AppColors, windowWidth} from '../../utils/GlobalStyles';
 import PropTypes from 'prop-types';
@@ -15,48 +15,62 @@ export const InputBox = styled.TextInput`
   overflow-y: hidden;
 `;
 
-export const BorderBottomInput = ({
-  value,
-  onChangeText,
-  placeholder,
-  maxLength,
-  secureTextEntry,
-  autoFocus,
-  onSubmitEditing,
-  width,
-  margin = {marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0},
-  padding = {paddingTop: 10, paddingBottom: 10, paddingLeft: 10, paddingRight: 10},
-  fontSize = 17,
-  disable = false,
-}) => {
-  return (
-    <InputContainer width={width}>
-      <InputBox
-        maxLength={maxLength}
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry}
-        autoFocus={autoFocus}
-        onSubmitEditing={onSubmitEditing}
-        placeholderTextColor={AppColors.Gray600}
-        editable={!disable}
-        color={disable && AppColors.Gray600}
-        fontSize={fontSize}
-        style={{
-          marginTop: margin.marginTop,
-          marginBottom: margin.marginBottom,
-          marginLeft: margin.marginLeft,
-          marginRight: margin.marginRight,
-          paddingTop: padding.paddingTop,
-          paddingBottom: padding.paddingBottom,
-          paddingLeft: padding.paddingLeft,
-          paddingRight: padding.paddingRight,
-        }}
-      />
-    </InputContainer>
-  );
-};
+export const BorderBottomInput = forwardRef(
+  (
+    {
+      value,
+      onChangeText,
+      placeholder,
+      maxLength,
+      secureTextEntry,
+      autoFocus,
+      onSubmitEditing,
+      blurOnSubmit,
+      width,
+      margin = {marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0},
+      padding = {
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+      },
+      fontSize = 17,
+      disable = false,
+    },
+    ref,
+  ) => {
+    console.log('ref : ', ref);
+    return (
+      <InputContainer width={width}>
+        <InputBox
+          maxLength={maxLength}
+          placeholder={placeholder}
+          value={value}
+          onChangeText={onChangeText}
+          secureTextEntry={secureTextEntry}
+          autoFocus={autoFocus}
+          onSubmitEditing={onSubmitEditing}
+          ref={ref}
+          blurOnSubmit={blurOnSubmit}
+          placeholderTextColor={AppColors.Gray600}
+          editable={!disable}
+          color={disable && AppColors.Gray600}
+          fontSize={fontSize}
+          style={{
+            marginTop: margin.marginTop,
+            marginBottom: margin.marginBottom,
+            marginLeft: margin.marginLeft,
+            marginRight: margin.marginRight,
+            paddingTop: padding.paddingTop,
+            paddingBottom: padding.paddingBottom,
+            paddingLeft: padding.paddingLeft,
+            paddingRight: padding.paddingRight,
+          }}
+        />
+      </InputContainer>
+    );
+  },
+);
 
 BorderBottomInput.propTypes = {
   value: PropTypes.string,
